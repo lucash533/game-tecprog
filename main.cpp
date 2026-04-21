d25804dbb8a90e0ff0e2831245cb4c2ac2205cb7
Lucas <veiga.2005@alunos.utfpr.edu.br>
2026-04-21 19:22:22 -0300

    Test: testing branch

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
     cout << "Hello World!" << endl;
 
+    int a = 0;
+    cin >> a;
+    cout << a * 2 << endl;
+
     cout << "Goodbye World..." << endl;
 
     return 0;
