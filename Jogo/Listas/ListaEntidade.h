fa9faf60bcf80d3862aee81eb101713d3a8e8dff
Vinicius-Takaki <vinitakaki17@gmail.com>
2026-06-02 18:08:04 -0300

    correção erro

---
+++ Jogo/Listas/ListaEntidade.h
@@ -0,0 +1,15 @@
+/*#pragma once
+#include "../Listas/Lista.h"
+#include "../Entidades/Entidade.h"
+class ListaEntidade
+{
+private:
+	Jogo::Lista<Entidade> LEs;
+public:
+	ListaEntidade();
+	~ListaEntidade();
+	void incluir(Entidade* pE, bool d);
+	void percorrer();
+};
+*/
+
