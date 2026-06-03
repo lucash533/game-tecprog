e8e623b6f6fb907dff74ebc2a33bac0dd71057ba
Lucas <veiga.2005@alunos.utfpr.edu.br>
2026-06-03 11:32:17 -0300

    feat: personagem adaptado para acomodar namespace

--- Jogo/Entidades/Personagens/Personagem.cpp
+++ Jogo/Entidades/Personagens/Personagem.cpp
@@ -1,31 +1,32 @@
 #include "Personagem.h"
 
-Personagem::Personagem() : num_vidas(3), velY(0.f), noChao(false) {
-}
-
-Personagem::~Personagem() {}
-
-void Personagem::salvarDataBuffer() {}
-
-void Personagem::aplicarGravidade() {
-    if (!noChao) {
-		velY += 0.5f; // acelera para baixo enquanto estiver fora do chão
+namespace Jogo {
+    Personagem::Personagem() : num_vidas(3), velY(0.f), noChao(false) {
     }
-    corpo.move(0.f, velY); // aplica a velocidade vertical no corpo
-    noChao = false; // reseta noChao
-}
+
+    Personagem::~Personagem() {}
+
+    void Personagem::salvarDataBuffer() {}
+
+    void Personagem::aplicarGravidade() {
+        if (!noChao) {
+            velY += 0.5f; // acelera para baixo enquanto estiver fora do chão
+        }
+        corpo.move(0.f, velY); // aplica a velocidade vertical no corpo
+        noChao = false; // reseta noChao
+    }
 
 
-void Personagem::setPosition(sf::Vector2f pos) {
-    corpo.setPosition(pos); // Reposiciona o corpo e sincroniza x,y 
-    x = pos.x;
-    y = pos.y;
+    void Personagem::setPosition(sf::Vector2f pos) {
+        corpo.setPosition(pos); // Reposiciona o corpo e sincroniza x,y 
+        x = pos.x;
+        y = pos.y;
+    }
+    void Personagem::pousar() {
+        velY = 0.f;
+        noChao = true;
+    }
 }
-void Personagem::pousar() {
-	velY = 0.f;
-	noChao = true;
-}
-
 
 
 
