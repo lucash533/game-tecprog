884c6252f4620a079f0db99d71a0fe665b34bdc9
Lucas <veiga.2005@alunos.utfpr.edu.br>
2026-06-03 12:03:56 -0300

    feat: classe armadilha adpatada para acomodar namespace

--- Jogo/Entidades/Obstaculos/Armadilha.cpp
+++ Jogo/Entidades/Obstaculos/Armadilha.cpp
@@ -1,22 +1,24 @@
 #include "Armadilha.h"
 
-Armadilha::Armadilha(float x, float y, float largura, float altura):danosidade(1) {
-	corpo.setSize(sf::Vector2f(largura, altura));
-	corpo.setPosition(x, y);
-	corpo.setFillColor(sf::Color::Red);
+namespace Jogo {
+	Armadilha::Armadilha(float x, float y, float largura, float altura):danosidade(1) {
+		corpo.setSize(sf::Vector2f(largura, altura));
+		corpo.setPosition(x, y);
+		corpo.setFillColor(sf::Color::Red);
 
-	danoso = true; // Armadilha é danosa
-}
-Armadilha::~Armadilha() {}
-void Armadilha::executar() 
-{
+		danoso = true; // Armadilha é danosa
+	}
+	Armadilha::~Armadilha() {}
+	void Armadilha::executar() 
+	{
 
-}
-void Armadilha::obstacularizar(Jogador* p) {
-	
-	//alguma forma de diminuir a vida do jogador
-	p->atordoamento();
-}
-void Armadilha::salvar() {
-	// Implementação do método salvar
+	}
+	void Armadilha::obstacularizar(Jogador* p) {
+		
+		//alguma forma de diminuir a vida do jogador
+		p->atordoamento();
+	}
+	void Armadilha::salvar() {
+		// Implementação do método salvar
+	}
 }
--- Jogo/Entidades/Obstaculos/Armadilha.h
+++ Jogo/Entidades/Obstaculos/Armadilha.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "Obstaculo.h"
-class Armadilha :
-    public Obstaculo
-{
-protected:
-    short int danosidade;
-public:
-	Armadilha(float x, float y, float largura, float altura = 20.f);
-	~Armadilha();
-	void executar();
-	void obstacularizar(Jogador* p); //a reação do jogador ao pisar na armadilha é artodoar o jogador e dar dano
-	void salvar();
-};
 
+namespace Jogo {
+	class Armadilha :
+		public Obstaculo {
+	protected:
+		short int danosidade;
+	public:
+		Armadilha(float x, float y, float largura, float altura = 20.f);
+		~Armadilha();
+		void executar();
+		void obstacularizar(Jogador* p); //a reação do jogador ao pisar na armadilha é artodoar o jogador e dar dano
+		void salvar();
+	};
+}
