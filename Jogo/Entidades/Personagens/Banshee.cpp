cd4ad08429d812f282eab365eb128f7faaf35bf1
Lucas <veiga.2005@alunos.utfpr.edu.br>
2026-06-03 11:50:21 -0300

    feat: classe banshee adaptada para acomodar namespace

--- Jogo/Entidades/Personagens/Banshee.cpp
+++ Jogo/Entidades/Personagens/Banshee.cpp
@@ -2,31 +2,33 @@
 //
 //A DEFINIRR
 //
-Banshee::Banshee() : raio(300.0f), alvo(nullptr), tamanho(40.0f)
-{
-	corpo.setSize(sf::Vector2f(tamanho, tamanho));
-	corpo.setFillColor(sf::Color::Green);
-	corpo.setPosition(sf::Vector2f(100, 100)); // posição inicial padrão
-	vel = sf::Vector2f(3.0f, 0.0f); // velocidade horizontal base
-}
-Banshee::~Banshee() {}
+namespace Jogo {
+	Banshee::Banshee() : raio(300.0f), alvo(nullptr), tamanho(40.0f)
+	{
+		corpo.setSize(sf::Vector2f(tamanho, tamanho));
+		corpo.setFillColor(sf::Color::Green);
+		corpo.setPosition(sf::Vector2f(100, 100)); // posição inicial padrão
+		vel = sf::Vector2f(3.0f, 0.0f); // velocidade horizontal base
+	}
+	Banshee::~Banshee() {}
 
-void Banshee::executar()
-{
-}
+	void Banshee::executar()
+	{
+	}
 
-void Banshee::mover()
-{
-}
+	void Banshee::mover()
+	{
+	}
 
-void Banshee::perseguir(sf::Vector2f pos_alvo, sf::Vector2f pos_inimigo)
-{
-}
+	void Banshee::perseguir(sf::Vector2f pos_alvo, sf::Vector2f pos_inimigo)
+	{
+	}
 
-void Banshee::danificar(Jogador* p)
-{
-}
+	void Banshee::danificar(Jogador* p)
+	{
+	}
 
-void Banshee::salvar()
-{
+	void Banshee::salvar()
+	{
+	}
 }
--- Jogo/Entidades/Personagens/Banshee.h
+++ Jogo/Entidades/Personagens/Banshee.h
@@ -3,21 +3,23 @@
 //
 // A DEFINIRRR
 //
-class Banshee :
-    public Inimigo
-{
-private:
-    float raio; // distância máxima para perseguir o jogador
-    Personagem* alvo; // ponteiro para o jogador (alvo)
-    float tamanho;
-public:
-	Banshee();
-	Banshee(float x, float y);
-	~Banshee();
-	void executar();
-	void mover();
-	void perseguir(sf::Vector2f pos_alvo, sf::Vector2f pos_inimigo);
-	void danificar(Jogador* p); // método para causar dano ao jogador
-	void salvar();
-};
 
+namespace Jogo {
+	class Banshee :
+		public Inimigo
+	{
+	private:
+		float raio; // distância máxima para perseguir o jogador
+		Personagem* alvo; // ponteiro para o jogador (alvo)
+		float tamanho;
+	public:
+		Banshee();
+		Banshee(float x, float y);
+		~Banshee();
+		void executar();
+		void mover();
+		void perseguir(sf::Vector2f pos_alvo, sf::Vector2f pos_inimigo);
+		void danificar(Jogador* p); // método para causar dano ao jogador
+		void salvar();
+	};
+}
