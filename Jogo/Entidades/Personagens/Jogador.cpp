5b8858238650dce3a1df004eea5bb20bdc5e5875
Lucas <veiga.2005@alunos.utfpr.edu.br>
2026-06-03 11:36:55 -0300

    feat: jogador adaptado para acomodar namespace

--- Jogo/Entidades/Personagens/Jogador.cpp
+++ Jogo/Entidades/Personagens/Jogador.cpp
@@ -2,43 +2,46 @@
 #include "Inimigo.h"
 #include <SFML/Graphics.hpp>
 
-Jogador::Jogador() : pontos(0) {
-    corpo.setSize(sf::Vector2f(40, 40));
-    corpo.setFillColor(sf::Color::Green);
-    corpo.setPosition(sf::Vector2f(100.f, 275.f));
+namespace Jogo {
+    Jogador::Jogador() : pontos(0) {
+        corpo.setSize(sf::Vector2f(40, 40));
+        corpo.setFillColor(sf::Color::Green);
+        corpo.setPosition(sf::Vector2f(100.f, 275.f));
 
-    //velocidade horizontal 5.0f
-    vel = sf::Vector2f(5.0f, 0.0f);
-}
-
-Jogador::~Jogador() {}
-
-void Jogador::executar() { mover(); }
-
-void Jogador::mover() {
-    // Movimentação horizontal
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) //Referencia: https://www.youtube.com/watch?v=WrJpux-vpcI&list=PLR17O9xbTbIBBoL3lli44N8LdZVvg-_uZ&index=2
-        corpo.move(-vel.x, 0.0f);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-        corpo.move(vel.x, 0.0f);
-
-    // Pulo (Teclando W e estando no chão)
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && noChao) {
-        velY = -12.0f;  // Força que empurra o jogador para cima apenas quando estiver no chão
-        noChao = false; // saiu do chão
+        //velocidade horizontal 5.0f
+        vel = sf::Vector2f(5.0f, 0.0f);
     }
 
-    // Aplica a gravidade sempre 
-    aplicarGravidade();
-}
-void Jogador::diminuirVelocidade() {
-	vel *= 0.5f; // Diminui a velocidade pela metade
-}
-void Jogador::atordoamento() {
-	
-    vel = sf::Vector2f(0.f, 0.f);
-    // Zera a velocidade para simular atordoamento, MASSS eu tenho q achar alguma forma para o jogador se recuperar, com algum tipo de temporizador
-}
+    Jogador::~Jogador() {}
 
-void Jogador::salvar() {}
-void Jogador::colidir(Inimigo* pIn) {}
+    void Jogador::executar() { mover(); }
+
+    void Jogador::mover() {
+        // Movimentação horizontal
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) //Referencia: https://www.youtube.com/watch?v=WrJpux-vpcI&list=PLR17O9xbTbIBBoL3lli44N8LdZVvg-_uZ&index=2
+            corpo.move(-vel.x, 0.0f);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            corpo.move(vel.x, 0.0f);
+
+        // Pulo (Teclando W e estando no chão)
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && noChao) {
+            velY = -12.0f;  // Força que empurra o jogador para cima apenas quando estiver no chão
+            noChao = false; // saiu do chão
+        }
+
+        // Aplica a gravidade sempre 
+        aplicarGravidade();
+    }
+    void Jogador::diminuirVelocidade() {
+        vel *= 0.5f; // Diminui a velocidade pela metade
+    }
+    void Jogador::atordoamento() {
+        
+        vel = sf::Vector2f(0.f, 0.f);
+        // Zera a velocidade para simular atordoamento, MASSS eu tenho q achar alguma forma para o jogador se recuperar, com algum tipo de temporizador
+    }
+
+    void Jogador::salvar() {}
+    void Jogador::colidir(Inimigo* pIn) {}
+
+}
--- Jogo/Entidades/Personagens/Jogador.h
+++ Jogo/Entidades/Personagens/Jogador.h
@@ -2,22 +2,22 @@
 #include "Personagem.h"
 #include <SFML/Graphics.hpp>
 
-class Inimigo; 
-class Jogador : public Personagem {
-public:
-    int pontos;
-    Inimigo* pIn; // ponteiro para inimigo (usado na colisão futuramente)
+namespace Jogo {
+    class Inimigo; 
+    class Jogador : public Personagem {
+    public:
+        int pontos;
+        Inimigo* pIn; // ponteiro para inimigo (usado na colisão futuramente)
 
-public:
-    Jogador();
-    ~Jogador();
-    void colidir(Inimigo* pIn); // reação ao colidir com inimigo
-    void salvar();
-    void mover(); // lê teclado e move o jogador
-    void executar();
-    void diminuirVelocidade(); //a forma que eu vi de diminuir a velocidade do jogador caso haja o efeito da lama
-    void atordoamento(); //efeito de atordoamento quando o jogador é preso na armadilha
-	
-};
-
-
+    public:
+        Jogador();
+        ~Jogador();
+        void colidir(Inimigo* pIn); // reação ao colidir com inimigo
+        void salvar();
+        void mover(); // lê teclado e move o jogador
+        void executar();
+        void diminuirVelocidade(); //a forma que eu vi de diminuir a velocidade do jogador caso haja o efeito da lama
+        void atordoamento(); //efeito de atordoamento quando o jogador é preso na armadilha
+        
+    };
+}
