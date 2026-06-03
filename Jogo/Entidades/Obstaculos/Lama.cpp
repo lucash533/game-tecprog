a103a175ab3f67cc54b7b8bfaa796e6ed0be55a9
Lucas <veiga.2005@alunos.utfpr.edu.br>
2026-06-03 12:02:28 -0300

    feat: classe lama adapatada para acomodar namespace

--- Jogo/Entidades/Obstaculos/Lama.cpp
+++ Jogo/Entidades/Obstaculos/Lama.cpp
@@ -1,20 +1,22 @@
 #include "Lama.h"
 
-Lama::Lama(float x, float y, float largura) : largura(30.f) {
-	corpo.setSize(sf::Vector2f(largura, 20.f));
-	corpo.setPosition(x, y);
-	corpo.setFillColor(sf::Color::Magenta);
-}
-Lama::~Lama() {}
-//diminuir a velocidade do jogador 
-void Lama::executar()
-{
-}
-//deve ser implementado no gerenciador de colisões uma forma de detectar a colisão do jogador com a lama para que ocorra o efeito
-void Lama::obstacularizar(Jogador* p)
-{
-	p->diminuirVelocidade();
-}
-void Lama::salvar() {
-	// Implementação do método salvar
+namespace Jogo {
+	Lama::Lama(float x, float y, float largura) : largura(30.f) {
+		corpo.setSize(sf::Vector2f(largura, 20.f));
+		corpo.setPosition(x, y);
+		corpo.setFillColor(sf::Color::Magenta);
+	}
+	Lama::~Lama() {}
+	//diminuir a velocidade do jogador 
+	void Lama::executar()
+	{
+	}
+	//deve ser implementado no gerenciador de colisões uma forma de detectar a colisão do jogador com a lama para que ocorra o efeito
+	void Lama::obstacularizar(Jogador* p)
+	{
+		p->diminuirVelocidade();
+	}
+	void Lama::salvar() {
+		// Implementação do método salvar
+	}
 }
