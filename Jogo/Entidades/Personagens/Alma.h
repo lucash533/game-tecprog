c37e26e7f930486fa9402bbd895d887313073f2b
Lucas <veiga.2005@alunos.utfpr.edu.br>
2026-06-03 11:57:22 -0300

    feat: classe alma adaptada para acomodar namespace

--- Jogo/Entidades/Personagens/Alma.h
+++ Jogo/Entidades/Personagens/Alma.h
@@ -1,25 +1,26 @@
 #pragma once
 #include "Inimigo.h"
 
-class Alma : public Inimigo
-{
-private:
-    float raio;  // distância máxima para detectar e perseguir o jogador
-	Jogador* alvo; // ponteiro para o jogador, usado para obter a posição do alvo
+namespace Jogo {
+    class Alma : public Inimigo
+    {
+    private:
+        float raio;  // distância máxima para detectar e perseguir o jogador
+        Jogador* alvo; // ponteiro para o jogador, usado para obter a posição do alvo
 
-public:
-	Alma(); // construtor padrão, vai precisar ser excluido com a criação de fases (APENAS PARA TESTES)
-	Alma(float x, float y);// construtor para criar inimigo em posição específica
-    ~Alma();
+    public:
+        Alma(); // construtor padrão, vai precisar ser excluido com a criação de fases (APENAS PARA TESTES)
+        Alma(float x, float y);// construtor para criar inimigo em posição específica
+        ~Alma();
 
-    void executar();
-    void danificar(Jogador* p);
-    void salvar();
-    void mover();
+        void executar();
+        void danificar(Jogador* p);
+        void salvar();
+        void mover();
 
-    void perseguir(sf::Vector2f pos_alvo, sf::Vector2f pos_inimigo); // move em direção ao jogador
-	void moverAletoriamente(); // movimento aleatório quando o jogador não estiver no raio de detecção
-    void setAlvo(Jogador* jogador) { alvo = jogador; } // define quem perseguir
-};
-
+        void perseguir(sf::Vector2f pos_alvo, sf::Vector2f pos_inimigo); // move em direção ao jogador
+        void moverAletoriamente(); // movimento aleatório quando o jogador não estiver no raio de detecção
+        void setAlvo(Jogador* jogador) { alvo = jogador; } // define quem perseguir
+    };
+}
 
