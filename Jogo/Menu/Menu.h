b09151fd8351d10eef16f5a0eae262d78bdc2044
Lucas <veiga.2005@alunos.utfpr.edu.br>
2026-06-05 16:53:19 -0300

    feat: criando menu e nodo

---
+++ Jogo/Menu/Menu.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../Ente.h"
+#include "../Jogo.h"
+
+namespace Principal {
+    class Menu : public Ente {
+    private:
+        // Listas ou vetores separados de nodos para cada parte do menu
+    public:
+        //desenhar()
+        //executar()
+    };
+}
